3fe933c0822d4489a3705752b07fe7a636462498
Florian Hartmann <jscoderr@gmail.com>
2018-07-05 17:26:01 -0700

    Add more variables to RProp

--- optimizers/rprop.py
+++ optimizers/rprop.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 class RProp:
-    def __init__(self, learning_rate, num_features):
+    def __init__(self, learning_rate, num_features, min_value=0.0000001, max_value=50, alpha=1.2, beta=0.5):
         self.learning_rates = np.full(num_features, learning_rate)
-        self.min = 0.0000001
-        self.max = 50
-        self.a = 1.2
-        self.b = 0.5
+        self.min = min_value
+        self.max = max_value
+        self.a = alpha
+        self.b = beta
         
         self.t = 0
         self.last_gradient = None
@@ -18,6 +18,8 @@ class RProp:
                     self.learning_rates[i] = min(self.learning_rates[i] * self.a, self.max)
                 elif gradient[i] * self.last_gradient[i] < 0:
                     self.learning_rates[i] = max(self.learning_rates[i] * self.b, self.min)
+            
+        self.learning_rates = np.round(self.learning_rates)
             
         self.t += 1
         self.last_gradient = gradient
